f0256e87f75e5583f496409a50bc4d0b17d7c97e
Ray Quincy Richardson <idjitPhreak@gmail.com>
2013-03-20 16:04:59 -0700

    linux setup stuff added up

---
+++ assets/www/SetupLinuxRay.bat
@@ -0,0 +1,32 @@
+@ECHO ON
+
+REM Before you run this script, set ELF_USER and ELF_IP=YOUR_REMOTE_MACHINE (Elastic IP)
+REM Set up a Linux Box
+REM Set up two private keys
+REM Copy three scripts
+
+set User=%ELF_USER%
+set SshUser=%User%@%ELF_IP%
+set Home=/home/%User%
+set CreateBin=CreateBin.sh
+set SetupGitScript=SetupGitForFinal.sh
+set SetupSshScript=SetupSsh.sh
+set SetupNodeScript=SetupNode.sh
+
+REM Create Bin
+pscp -v %CreateBin% %SshUser%:%Home%/%CreateBin%
+plink -t %SshUser% /bin/chmod +x %Home%/%CreateBin%
+plink -t %SshUser% %Home%/%CreateBin%
+plink -t %SshUser% /bin/rm %Home%/%CreateBin%
+
+REM Copy Scripts
+pscp %SetupSshScript% %SshUser%:%Home%/bin/%SetupSshScript%
+pscp %SetupGitScript% %SshUser%:%Home%/bin/%SetupGitScript%
+pscp %SetupNodeScript% %SshUser%:%Home%/bin/%SetupNodeScript%
+plink -t %SshUser% /bin/chmod +x %Home%/bin/%SetupSshScript%
+plink -t %SshUser% /bin/chmod +x %Home%/bin/%SetupGitScript%
+plink -t %SshUser% /bin/chmod +x %Home%/bin/%SetupNodeScript%
+
+REM Run Scripts 
+plink -t %SshUser% %Home%/bin/%SetupNodeScript%
+plink -t %SshUser% %Home%/bin/%SetupGitScript%
